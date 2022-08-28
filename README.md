94df128c4f07b0b89c4222e517d35255c5d5987d
Hebert Ribeiro de Freitas <hebertrfreitas@gmail.com>
2022-08-27 21:13:38 -0300

    active dymamodb stream in table

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 
 Projeto para exemplificar o uso do [dynamodb streams](https://docs.aws.amazon.com/pt_br/amazondynamodb/latest/developerguide/Streams.html)
 
-
--- docker/dynamodb-entrypoint.sh
+++ docker/dynamodb-entrypoint.sh
@@ -3,5 +3,6 @@ echo "Criando tabela"
   --table-name User \
   --attribute-definitions AttributeName=Id,AttributeType=S \
   --key-schema AttributeName=Id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5
+  --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5 \
+  --stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
   echo "Tabela criada com sucesso"
