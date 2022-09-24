236dc9af9375077cf30b25bb2b24a805a9a673d3
Hebert Ribeiro de Freitas <hebertrfreitas@gmail.com>
2022-09-24 19:11:55 -0300

    fix makefile and lambda

--- lambda/users_lambda.py
+++ lambda/users_lambda.py
@@ -2,19 +2,27 @@ import os
 import logging
 import jsonpickle
 import boto3
+import sys
 #from aws_xray_sdk.core import xray_recorder
 #from aws_xray_sdk.core import patch_all
 
 logger = logging.getLogger()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 logger.setLevel(logging.INFO)
 #patch_all()
 
 client = boto3.client('lambda')
-client.get_account_settings()
+#client.get_account_settings()
 
 def handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
     logger.info('## EVENT\r' + jsonpickle.encode(event))
     logger.info('## CONTEXT\r' + jsonpickle.encode(context))
-    response = client.get_account_settings()
-    return response['AccountUsage']
+#    response = client.get_account_settings()
+#    return response['AccountUsage']
--- makefile
+++ makefile
@@ -14,13 +14,20 @@ down:
 ## create virtual environment and install pip dependecies
 lambda-setup:
 	rm -rf lambda/.venv; \
+	rm -rf lambda/package; \
 	python3 -m venv lambda/.venv; \
 	source lambda/.venv/bin/activate; \
-	pip install -r lambda/requirements.txt;
+	pip install -r lambda/requirements.txt; \
+	deactivate;
 	
-
 ## create a zip with the python script
 lambda-zip:
-	zip -j docker/lambda/users-lambda.zip $(LAMBDA_FILE) lambda/requirements.txt
+	cd lambda/.venv/lib/python3.8/site-packages/; \
+	zip -r ../../../../../docker/lambda/users-lambda.zip .; \
+	cd ../../../../; \
+	zip -g ../docker/lambda/users-lambda.zip users_lambda.py requirements.txt;
+##	zip -j docker/lambda/users-lambda.zip lambda/users_lambda.py lambda/requirements.txt; \
+##	zip -ur docker/lambda/users-lambda.zip lambda/package/; 
+
 
 
